e418b3375537eddc6f0744e10adc88b011dd21d4
Rafael Martins <rafael@arvore.com.br>
2022-10-30 15:30:01 -0300

    secrets twitter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cgi import test
+import secrets
 import requests
 import json
 # import pandas as pd
@@ -8,6 +9,20 @@ from datetime import datetime
 import csv
 import tweepy
 
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+BEARER_TOKEN = os.environ['BEARER_TOKEN']
+
+api = tweepy.Client(
+    bearer_token=BEARER_TOKEN,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET,
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET
+)
+
 
 def imprimir():
     data = requests.get(
@@ -15,30 +30,24 @@ def imprimir():
 
     json_data = json.loads(data.content)
 
-    candidato = []
-    # partido = []
-    votos = []
-    porcentagem = []
-
     tt = []
 
     currentDateAndTime = datetime.now()
     currentTime = currentDateAndTime.strftime("%H:%M:%S")
 
+    tt.append("Resultados das eleições presidenciais")
     tt.append(f"Horário: {currentTime}")
     tt.append(f"Urnas apuradas: {json_data['pst']}%")
 
-    for informacoes in json_data['cand']:
+    for tse in json_data['cand']:
 
-        if informacoes['seq'] in ['1', '2', '3', '4']:
+        if tse['seq'] in ['1', '2', '3', '4']:
 
-            candidato.append(informacoes['nm'])
-
-            c = informacoes['nm'].replace("JAIR BOLSONARO", "Geno&Cida")
-            v = informacoes['vap']
-            p = informacoes['pvap'] + ' %'
+            c = tse['nm'].replace("JAIR BOLSONARO", "inominável")
+            v = tse['vap']
+            p = tse['pvap'] + ' %'
             tt.append("")
-            tt.append(f"{c} - Votos:{v} - {p}")
+            tt.append(f"{c} - votos:{v} - {p}")
 
             fields = [currentTime, c, v, p]
 
@@ -46,13 +55,11 @@ def imprimir():
                 writer = csv.writer(f)
                 writer.writerow(fields)
 
-    print(*tt, sep='\n')
+    tt.append("#LulaPresidente2022, #Eleicao2022")
+    api.create_tweet(text='\n'.join(tt))
 
-    time.sleep(10)
+    time.sleep(1)
     os.system('clear')
     os.system('cat resultado-hora-a-hora.csv')
-    print('\n')
-    # imprimir()
-
 
 imprimir()
