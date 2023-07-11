f6c421eb12c5a7d38e047f4ce554b9577bd7969e
Rafael Martins <rafael@arvore.com.br>
2023-07-11 16:27:26 -0300

    Create Tweet app to post

--- amazon.py
+++ amazon.py
@@ -2,28 +2,36 @@ import os
 import logging
 import tweepy
 
-# Twitter API credentials
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-BEARER_TOKEN = os.environ['BEARER_TOKEN']
+# Configure the logging module
+logging.basicConfig(level=logging.ERROR)  # Set the logging level to ERROR
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
 
 
-# Authenticate to Twitter
-api = tweepy.Client(
-    bearer_token=BEARER_TOKEN,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET,
-    consumer_key=CONSUMER_KEY,
-    consumer_secret=CONSUMER_SECRET
-)
+def authenticate():
+    CONSUMER_KEY = os.environ['CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+    ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+    BEARER_TOKEN = os.environ['BEARER_TOKEN']
+    
+    return tweepy.Client(
+        bearer_token=BEARER_TOKEN,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET,
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET
+    )
 
-# Create a tweet
-tt = []
-tt.append("Teste")
-tt.append("Segundo Teste")
-tt.append("Terceiro Teste com hastag")
+def tweet_text():
+    tt = []
+    tt.append("Teste")
+    tt.append("Segundo Teste")
+    tt.append("Terceiro Teste com hastag")
+    return tt
 
-# Post the tweet
-api.create_tweet(text='\n'.join(tt))
+if __name__ == "__main__":
+    api = authenticate
+    api.create_tweet(text='\n'.join(tweet_text))
+    
